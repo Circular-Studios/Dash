2424e90fdceab60257fffda07e00f68d0a7d600f
Colden Cullen <ColdenCullen@gmail.com>
2013-12-10 16:48:34 -0500

    Setup initial project, added graphosGame file.

---
+++ source/app.d
@@ -0,0 +1,8 @@
+import core.graphosGame;
+import std.stdio;
+
+void main()
+{
+	//auto g = new GraphosGame();
+	writeln( "POOP" );
+}
---
+++ source/core/Main.cpp
@@ -0,0 +1,8 @@
+#include "ScriptedGraphosGame.h"
+
+int main()
+{
+	Graphos::Core::ScriptedGraphosGame game;
+
+	game.Run();
+}

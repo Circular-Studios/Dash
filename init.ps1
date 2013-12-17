2c7085f110996b381897be7c3f6dd739b49c9b8a
Colden Cullen <ColdenCullen@gmail.com>
2013-12-17 10:18:16 -0500

    Switched D-YAML to be a dub package again, now that fixes are made.

--- init.ps1
+++ init.ps1
@@ -1,4 +1,5 @@
 dub fetch --system derelict-util --version=1.0.0
 dub fetch --system derelict-gl3 --version=~master
 dub fetch --system derelict-fi --version=~master
+dub fetch --system dyaml --version=~master
 dub generate visuald

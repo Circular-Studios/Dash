dea57c732ea0bd55a84f93a34ff499c6af58a863
Colden Cullen <ColdenCullen@gmail.com>
2013-12-27 15:46:45 -0500

    Fixed use of depricated app.d

--- source/app.d
+++ source/app.d
@@ -1,12 +1,7 @@
 import core.dgame;
 import scripting.scripts;
 
-import myclass;
-
 void main()
 {
-	Scripts.initialize();
-	Scripts.callFunction!(MyClass)( getDGame.mangleof );
-
     new DGame().run();
 }

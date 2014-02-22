7bc3bedb55640a4d821093e9385bc2d87790e5a9
Tyler Wozniak <ttw6741@rit.edu>
2014-02-21 22:25:13 -0500

    Fixed fixed merge.

--- source/components/camera.d
+++ source/components/camera.d
@@ -12,15 +12,10 @@ import std.signals, std.conv;
 
 final class Camera : Component
 {
-public:
-	/**
-	 * The view matrix of the camera.
-	 */
-	mixin DirtyProperty!( "mat4", "viewMatrix", "updateViewMatrix" );
-	
+public:	
 	mixin Signal!( string, string );
 
-	this()
+	this(  )
 	{
 		super( null );
 	}
@@ -28,68 +23,39 @@ public:
 	override void update() { }
 	override void shutdown() { }
 
-	static mat4 lookAtLH( vec3 cameraTarget, vec3 cameraUpVector, vec3 cameraPosition )
+	final @property mat4 viewMatrix()
 	{
-		auto zaxis = ( cameraTarget - cameraPosition ).normalized();
-		auto xaxis = cross( cameraUpVector, zaxis ).normalized();
-		auto yaxis = cross( zaxis, xaxis );
+		if( _viewMatrixIsDirty )
+			updateViewMatrix();
 
-		mat4 newMatrix;
-
-		newMatrix.matrix[0][0] = xaxis.x;
-		newMatrix.matrix[1][0] = xaxis.y;
-		newMatrix.matrix[2][0] = xaxis.z;
-		newMatrix.matrix[0][1] = yaxis.x;
-		newMatrix.matrix[1][1] = yaxis.y;
-		newMatrix.matrix[2][1] = yaxis.z;
-		newMatrix.matrix[0][2] = zaxis.x;
-		newMatrix.matrix[1][2] = zaxis.y;
-		newMatrix.matrix[2][2] = zaxis.z;
-		newMatrix.matrix[3][0] = -( xaxis * cameraPosition );
-		newMatrix.matrix[3][1] = -( yaxis * cameraPosition );
-		newMatrix.matrix[3][2] = -( zaxis * cameraPosition );
-		newMatrix.matrix[3][3] = 1.0f;
-
-		return newMatrix;
-	}
-
-	static mat4 buildPerspective( const float fov, const float screenAspect, const float near, const float depth )
-	{
-		mat4 toReturn;
-		toReturn.clear( 0 );
-		auto yScale = 1 / tan( fov / 2 );
-		auto xScale = yScale / screenAspect;
-
-		toReturn[ 0 ][ 0 ] = xScale;
-		toReturn[ 1 ][ 1 ] = yScale;
-		toReturn[ 2 ][ 2 ] = depth / ( depth - near );
-		toReturn[ 2 ][ 3 ] = 1.0f;
-		toReturn[ 3 ][ 2 ] = ( -near * depth ) / ( depth - near );
-		toReturn[ 3 ][ 3 ] = 0.0f;
-
-		return toReturn.transposed();
+		return _viewMatrix;
 	}
 
 private:
+	mat4 _viewMatrix;
+	bool _viewMatrixIsDirty;
 	final void setMatrixDirty( string prop, string newVal )
 	{
 		_viewMatrixIsDirty = true;
 	}
 	final void updateViewMatrix()
 	{
-		/*auto up = owner.transform.rotation.to_matrix( 4, 4 ) * new vec3( 0, 1, 0 );
-		auto lookAt = ( owner.transform.rotation.to_matrix( 4, 4 ) * new vec3( 0, 0, 1 ) ) + owner.transform.position;
+		//Assuming pitch & yaw are in radians
+		float cosPitch = cos( owner.transform.rotation.pitch );
+		float sinPitch = sin( owner.transform.rotation.pitch );
+		float cosYaw = cos( owner.transform.rotation.yaw );
+		float sinYaw = sin( owner.transform.rotation.yaw );
 
-		vec3[3] axes;
-		axes[ 2 ] = ( lookAt - owner.transform.position ).normalize();
-		axes[ 0 ] = up.cross( axes[ 2 ] ).normalize();
-		axes[ 1 ] = axes[ 2 ].cross( axes[ 0 ] ).normalize();
-		
-		for( uint ii = 0; ii < 3; ++ii )
-		{
-			for( uint jj = 0; jj < 3; ++jj )
-				viewMatrix[ jj ][ ii ] = axes[ ii ][ jj ];
-			viewMatrix[ 3 ][ ii ] = -axes[ ii ].dot( owner.transform.position );
-		}*/
+		vec3 xaxis = vec3( cosYaw, 0.0f, -sinYaw );
+		vec3 yaxis = vec3( sinYaw * sinPitch, cosPitch, cosYaw * sinPitch );
+		vec3 zaxis = vec3( sinYaw * cosPitch, -sinPitch, cosPitch * cosYaw );
+
+		_viewMatrix.clear( 0.0f );
+		_viewMatrix[ 0 ] = [ xaxis.x, yaxis.x, zaxis.x, 0 ];
+		_viewMatrix[ 1 ] = [ xaxis.y, yaxis.y, zaxis.y, 0 ];
+		_viewMatrix[ 2 ] = [ xaxis.z, yaxis.z, zaxis.z, 0 ];
+		_viewMatrix[ 3 ] = [ -( xaxis * owner.transform.position ), -( yaxis * owner.transform.position ), -( zaxis * owner.transform.position ), 1 ];
+
+		_viewMatrixIsDirty = false;
 	}
 }

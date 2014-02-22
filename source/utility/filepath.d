110b331731ce4b8a250d78df8fd4b737913a8252
Colden Cullen <ColdenCullen@gmail.com>
2014-02-21 20:54:33 -0500

    Fixed compile error in unittest.

--- source/utility/filepath.d
+++ source/utility/filepath.d
@@ -167,7 +167,7 @@ unittest
 	import std.stdio;
 	writeln( "Dash FilePath properties unittest" );
 
-	auto fp = new FilePath( FilePath.Resources.Config );
+	auto fp = new FilePath( FilePath.Resources.ConfigFile );
 
 	assert( fp.fileName == "Config.yml", "FilePath.fileName error." );
 	assert( fp.baseFileName == "Config", "FilePath.baseFileName error." );

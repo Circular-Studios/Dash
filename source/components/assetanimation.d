5755f8ae9bfb7faa3279e37e2425daa6b35cb441
Timothy Reynolds <tmr9209@rit.edu>
2014-02-25 17:33:41 -0500

    Cleanup and fixed a parent bug when creating bones.

--- source/components/assetanimation.d
+++ source/components/assetanimation.d
@@ -17,26 +17,27 @@ public:
 	{
 		_animationSet.duration = cast(float)animation.mDuration;
 		_animationSet.fps = cast(float)animation.mTicksPerSecond;
-
-		_animationSet.boneAnimData = makeBoneFromNode( animation, boneHierarchy );
-	}
-
-	// PROBLEM! To get the parent you cant call makeBoneFromNode, otherwise it will be extra new nodes. Instead pass in parent node (if there is one)
-	Bone makeBoneFromNode( const(aiAnimation*) animation, const(aiNode*) bones )
-	{
-		Bone temp = new Bone( cast(string)bones.mName.data );
 		
-		if( !(bones.mParent !is null) )
-			temp.parent = makeBoneFromNode( animation, bones.mParent );
-
+		_animationSet.boneAnimData = makeBonesFromNode( animation, boneHierarchy, null );
+	}
+	
+	// PROBLEM! To get the parent you cant call makeBoneFromNode, otherwise it will be extra new nodes. Instead pass in parent node (if there is one)
+	Bone makeBonesFromNode( const(aiAnimation*) animation, const(aiNode*) bones, Bone parent )
+	{
+		Bone bone = new Bone();
+		bone.name = cast(string)bones.mName.data;
+		
+		if( parent !is null )
+			bone.parent = parent;
+		
 		for(int i = 0; i < bones.mNumChildren; i++)
 		{
-			temp.children ~= makeBoneFromNode( animation, bones.mChildren[i]);
+			bone.children ~= makeBonesFromNode( animation, bones.mChildren[i], bone );
 		}
-
-		assignCorrectAnimationData( animation, temp );
-
-		return temp;
+		
+		assignCorrectAnimationData( animation, bone );
+		
+		return bone;
 	}
 	void assignCorrectAnimationData( const(aiAnimation*) animation, Bone boneToAssign )
 	{
@@ -59,14 +60,14 @@ public:
 	// Go through array of keys and convert/store in vector[]
 	vec3[] convertVectorArray(const(aiVectorKey*) vectors, int numKeys)
 	{
-		vec3[] temp;
-		for(int i = 0; i < numKeys; i++)
+		vec3[] keys;
+		for( int i = 0; i < numKeys; i++ )
 		{
-			aiVector3D vector = vectors[i].mValue;
-			temp ~= vec3(vector.x, vector.y, vector.z);
+			aiVector3D vector = vectors[ i ].mValue;
+			keys ~= vec3(vector.x, vector.y, vector.z);
 		}
 
-		return temp;
+		return keys;
 	}
 	
 	// Find bone with name in our structure
@@ -102,11 +103,6 @@ public:
 	}
 	class Bone
 	{
-		this( string boneName )
-		{
-			name = boneName;
-		}
-
 		string name;
 		Bone parent;
 		Bone[] children;

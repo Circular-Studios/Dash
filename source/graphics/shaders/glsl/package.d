1f20e6af7261e5f370cee1e6ebe216e0ef91b21d
Eric Christenson <esc6491@rit.edu>
2014-03-06 11:39:49 -0500

    Moved animatedgeometry glsl into its own file

--- source/graphics/shaders/glsl/package.d
+++ source/graphics/shaders/glsl/package.d
@@ -4,3 +4,4 @@ import graphics.shaders.glsl.ambientlight;
 import graphics.shaders.glsl.directionallight;
 import graphics.shaders.glsl.geometry;
 import graphics.shaders.glsl.pointlight;
+import graphics.shaders.glsl.animatedgeometry;
--- source/graphics/shaders/shaders.d
+++ source/graphics/shaders/shaders.d
@@ -298,38 +298,3 @@ public:
 	}
 }
 
-
-///
-/// Animated Geometry Shader
-///
-immutable string animatedGeometryVS = q{
-	#version 400
-
-	layout(location = 0) in vec3 vPosition_m;
-	layout(location = 1) in vec2 vUV;
-	layout(location = 2) in vec3 vNormal_m;
-	layout(location = 3) in vec3 vTangent_m;
-	layout(location = 4) in vec4 vBone_m;
-	layout(location = 5) in vec4 vWeight_m;
-
-	out vec4 fPosition_s;
-	out vec3 fNormal_w;
-	out vec2 fUV;
-	out vec3 fTangent_w;
-	out vec3 fBitangent_w;
-
-	uniform mat4 world;
-	uniform mat4 worldView;
-	uniform mat4 worldViewProj;
-
-	void main( void )
-	{
-		// gl_Position is like SV_Position
-		fPosition_s = worldViewProj * vec4( vPosition_m, 1.0f );
-		gl_Position = fPosition_s;
-		fUV = vUV;
-
-		fNormal_w = ( world * vec4( vNormal_m, 0.0f ) ).xyz;
-		fTangent_w =  ( world * vec4( vTangent_m, 0.0f ) ).xyz;
-	}
-};

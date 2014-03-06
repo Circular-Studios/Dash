bdb658f4418fd4f0499d3dd1175a905be2f177f3
Colden Cullen <ColdenCullen@gmail.com>
2014-03-06 14:51:27 -0500

    Fixed owner being static.

--- source/components/icomponent.d
+++ source/components/icomponent.d
@@ -12,16 +12,27 @@ import yaml;
 interface IComponent
 {
 public:
+	/**
+	 * Functions to call when creating components.
+	 */
 	static IComponent function( Node, GameObject )[string] initializers;
 
 	/**
 	 * The GameObject that owns this component.
 	 */
-	final @property ref GameObject owner()
+	final @property GameObject owner()
 	{
-		static GameObject owner;
-		
-		return owner;
+		GameObject* owner = this in owners;
+		if( owner )
+			return *owner;
+		else
+			return null;
+	}
+
+	/// ditto
+	final @property void owner( GameObject newOwner )
+	{
+		owners[ this ] = newOwner;
 	}
 
 	/**
@@ -33,3 +44,5 @@ public:
 	 */
 	void shutdown();
 }
+
+private GameObject[ IComponent ] owners;

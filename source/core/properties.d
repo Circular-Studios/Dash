19b3787e1a4a4963ae3c3e5b595c22dcca0228e6
Colden Cullen <ColdenCullen@gmail.com>
2014-03-28 18:32:30 -0400

    Rewrote properties in a much more readable format.

--- source/core/properties.d
+++ source/core/properties.d
@@ -6,6 +6,7 @@
 module core.properties;
 
 public import std.traits;
+import std.array;
 
 enum AccessModifier : string
 {
@@ -38,7 +39,50 @@ template Property( alias field, AccessModifier setterAccess = AccessModifier.Pro
  */
 template Getter( alias field, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
 {
-	enum Getter = "final " ~ access ~ " @property auto " ~ name ~ "() @safe pure nothrow { return " ~ field.stringof ~ ";}\n";
+	enum Getter = q{
+		final $access @property auto $name() @safe pure nothrow
+		{
+			return $field;
+		}}
+		.replace( "$field", field.stringof ).replace( "$access", cast(string)access ).replace( "$name", name );
+}
+
+/**
+ * Generates a getter for a field that can be marked as dirty. Calls updateFunc if is dirty.
+ * 
+ * Params:
+ * 	field = 				The field to generate the property for.
+ * 	updateFunc = 			The function to call when the function is dirty.
+ * 	access = 				The access modifier for the getter function.
+ * 	name = 					The name of the property functions. Defaults to the field name minus the first character. Meant for fields that start with underscores.
+ */
+template DirtyGetter( alias field, alias updateFunc, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
+	if( is( typeof(T) : IDirtyable ) )
+{
+	enum DirtyGetter = q{
+		final $access @property auto $name() @safe pure nothrow
+		{
+			if( $field.isDirty() )
+				$updateFunc();
+			return $field;
+		}}
+		.replace( "$field", field.stringof ).replace( "$updateFunc", updateFunc.stringof ).replace( "$access", cast(string)access ).replace( "$name", name );
+}
+
+/// ditto
+template DirtyGetter( alias field, alias updateFunc, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
+	if( !is( typeof(T) : IDirtyable ) )
+{
+	enum DirtyGetter = q{
+		$type $dirtyFieldName;
+		final $access @property auto $name() @safe pure nothrow
+		{
+			if( $field != $dirtyFieldName )
+				$updateFunc();
+			return $field;
+		}}
+		.replace( "$field", field.stringof ).replace( "$updateFunc", updateFunc.stringof ).replace( "$access", cast(string)access ).replace( "$name", name )
+		.replace( "$type", typeof(field).stringof ).replace( "$dirtyFieldName", "_" ~ field.stringof ~ "Prev" );
 }
 
 /**
@@ -51,5 +95,19 @@ template Getter( alias field, AccessModifier access = AccessModifier.Protected, 
  */
 template Setter( alias field, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
 {
-	enum Setter = "final " ~ access ~ " @property void " ~ name ~ "(" ~ typeof(field).stringof ~ " newVal) @safe pure nothrow { " ~ field.stringof ~ " = newVal;}\n";
+	enum Setter = q{
+		final $access @property void $name( $type newVal ) @safe pure nothrow
+		{
+			$field = newVal;
+		}}
+		.replace( "$field", field.stringof ).replace( "$access", cast(string)access ).replace( "$name", name )
+		.replace( "$type", typeof(field).stringof );
+}
+
+/**
+ * Requires implementation of the isDirty property.
+ */
+interface IDirtyable
+{
+	@property bool isDirty();
 }

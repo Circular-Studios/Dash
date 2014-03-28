fa736eeefb9d35abacc6ed8890d6e662a5f6034a
Colden Cullen <ColdenCullen@gmail.com>
2014-03-28 17:45:51 -0400

    Documented properties, removed old cruft.

--- source/core/properties.d
+++ source/core/properties.d
@@ -14,37 +14,42 @@ enum AccessModifier : string
 	Private = "private",
 }
 
+/**
+ * Generates a getter and setter for a field.
+ * 
+ * Params:
+ * 	field = 				The field to generate the property for.
+ * 	setterAccess = 			The access modifier for the setter function.
+ * 	getterAccess = 			The access modifier for the getter funciton.
+ * 	name = 					The name of the property functions. Defaults to the field name minus the first character. Meant for fields that start with underscores.
+ */
 template Property( alias field, AccessModifier setterAccess = AccessModifier.Protected, AccessModifier getterAccess = AccessModifier.Public, string name = field.stringof[ 1..$ ] )
 {
 	enum Property = Getter!( field, getterAccess, name ) ~ Setter!( field, setterAccess, name );
 }
 
+/**
+ * Generates a getter for a field.
+ * 
+ * Params:
+ * 	field = 				The field to generate the property for.
+ * 	access = 				The access modifier for the getter function.
+ * 	name = 					The name of the property functions. Defaults to the field name minus the first character. Meant for fields that start with underscores.
+ */
 template Getter( alias field, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
 {
 	enum Getter = "final " ~ access ~ " @property auto " ~ name ~ "() @safe pure nothrow { return " ~ field.stringof ~ ";}\n";
 }
 
-template DirtyGetter( alias field, string update, AccessModifier access = AccessModifier.Public, string name = field.stringof[ 1..$ ] )
-{
-	enum DirtyGetter =
-		"public bool " ~ field.stringof ~ "IsDirty = true;\n" ~
-		"final " ~ access ~ " @property auto " ~ name ~ "() @safe pure nothrow {" ~
-			"if(" ~ field.stringof ~ "IsDirty)" ~
-				update ~ "();"
-			"return " ~ field.stringof ~ ";}\n";
-}
-
-void setDirty( alias field )()
-{
-	mixin( field.stringof ~ "IsDirty = true;" );
-}
-
+/**
+ * Generates a setter for a field.
+ * 
+ * Params:
+ * 	field = 				The field to generate the property for.
+ * 	access = 				The access modifier for the setter function.
+ * 	name = 					The name of the property functions. Defaults to the field name minus the first character. Meant for fields that start with underscores.
+ */
 template Setter( alias field, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
 {
 	enum Setter = "final " ~ access ~ " @property void " ~ name ~ "(" ~ typeof(field).stringof ~ " newVal) @safe pure nothrow { " ~ field.stringof ~ " = newVal;}\n";
-}
-
-template DirtySetter( alias field, AccessModifier access = AccessModifier.Protected, string name = field.stringof[ 1..$ ] )
-{
-	enum Setter = "final " ~ access ~ " @property void " ~ name ~ "(" ~ typeof(field).stringof ~ " newVal) @safe pure nothrow { " ~ field.stringof ~ " = newVal; changed();}\n";
 }

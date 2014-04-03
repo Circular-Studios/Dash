2fe7a3aa496e411529083b1b2c3212998c735d95
Tyler Wozniak <ttw6741@rit.edu>
2014-04-03 11:55:07 -0400

    Directional light converted to view space

--- source/graphics/shaders/glsl/directionallight.d
+++ source/graphics/shaders/glsl/directionallight.d
@@ -9,13 +9,20 @@ immutable string directionallightVS = q{
 	layout(location = 0) in vec3 vPosition_s;
 	layout(location = 1) in vec2 vUV;
 
+	uniform mat4 invProj;
+
 	out vec4 fPosition_s;
 	out vec2 fUV;
+	out vec3 fViewRay;
 
 	void main( void )
 	{
 		fPosition_s = vec4( vPosition_s, 1.0f );
 		gl_Position = fPosition_s;
+
+		vec3 position_v = mul( invProj, vPosition_s );
+		// This is the ray clamped to depth 1, and it'll just be moved & interpolated in the XY
+		fViewRay = vec3( position_v.xy / position_v.z, 1.0f );
 		fUV = vUV;
 	}
 };
@@ -31,14 +38,17 @@ immutable string directionallightFS = q{
 
 	in vec4 fPosition;
 	in vec2 fUV;
+	in vec3 fViewRay;
 
 	// this diffuse should be set to the geometry output
 	uniform sampler2D diffuseTexture;
 	uniform sampler2D normalTexture;
 	uniform sampler2D depthTexture;
 	uniform DirectionalLight light;
-	uniform vec3 eyePosition_w;
-	uniform mat4 invViewProj;
+	// A pair of constants for reconstructing the linear Z
+	// [ (-Far * Near ) / ( Far - Near ),  Far / ( Far - Near )  ]
+	uniform vec2 projectionConstant;
+	uniform mat4 invProj;
 
 	// https://stackoverflow.com/questions/9222217/how-does-the-fragment-shader-know-what-variable-to-use-for-the-color-of-a-pixel
 	out vec4 color;
@@ -54,19 +64,21 @@ immutable string directionallightFS = q{
 	{
 		vec3 textureColor = texture( diffuseTexture, fUV ).xyz;
 		float specularIntensity = texture( diffuseTexture, fUV ).w;
-		vec3 normal = texture( normalTexture, fUV ).xyz;
-		vec3 lightDir = normalize( light.direction );
+		vec3 normal_v = texture( normalTexture, fUV ).xyz;
+		vec3 lightDir_v = normalize( light.direction );
+
+		// Reconstruct position from Depth
+		float depth = texture( depthTexture, fUV ).x;
+		float linearDepth = projectionConstant.x / ( depth - projectionConstant.y );
+		vec3 position_v = viewRay * linearDepth;
+
 
 		// Diffuse lighting calculations
 		float diffuseScale = clamp( dot( normal, -lightDir ), 0, 1 );
 
 		// Specular lighting calculations
-		// pixelPosition is essentially 3D screen space coordinates - x, y, z of the screen
-		vec3 pixelPosition_s = vec3( fUV.x * 2.0f - 1.0f, fUV.y * 2.0f - 1.0f, texture( depthTexture, fUV ).x );
-		// Multiplying screen space coordinates by the inverse viewProjection matrix gives you world coordinates
-		vec3 pixelPosition_w = ( invViewProj * vec4( pixelPosition_s, 1.0f ) ).xyz;
-		vec3 eyeDirection = normalize( ( pixelPosition_w - eyePosition_w).xyz );
-		float specularScale = clamp( dot( eyeDirection, reflect( -lightDir, normal ) ), 0, 1 );
+		// Usually in these you see an "eyeDirection" variable, but in view space that is our position
+		float specularScale = clamp( dot( position_v, reflect( -lightDir, normal ) ), 0, 1 );
 
 		vec3 diffuse = ( diffuseScale * light.color ) * textureColor;
 		// "8" is the reflectiveness

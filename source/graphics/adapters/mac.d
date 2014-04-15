abf6c3237f1c821c7eb9565ddccd33bc444c5113
Colden Cullen <ColdenCullen@gmail.com>
2014-04-15 19:02:41 -0400

    Updated mac adapter.

--- source/graphics/adapters/mac.d
+++ source/graphics/adapters/mac.d
@@ -17,53 +17,14 @@ final class Mac : Adapter
 public:
     static @property Mac get() { return cast(Mac)Graphics.adapter; }
 
-    override void initialize()
-    {
+    override void initialize() { }
+    override void shutdown() { }
+    override void resize() { }
+    override void reload() { }
+    override void swapBuffers() { }
 
-    }
+    override void openWindow() { }
+    override void closeWindow() { }
 
-    override void shutdown()
-    {
-
-    }
-
-    override void resize()
-    {
-
-    }
-
-    override void reload()
-    {
-
-    }
-
-    override void beginDraw()
-    {
-
-    }
-
-    override void drawObject( GameObject obj )
-    {
-
-    }
-
-    override void endDraw()
-    {
-
-    }
-
-    override void openWindow()
-    {
-
-    }
-
-    override void closeWindow()
-    {
-
-    }
-
-    override void messageLoop()
-    {
-
-    }
+    override void messageLoop() { }
 }

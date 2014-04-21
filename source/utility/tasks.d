4def0828aaad5628fc059a9dc79eda9f6cb17e8d
Colden Cullen <ColdenCullen@gmail.com>
2014-04-21 11:05:28 -0400

    Changed signature of timed tasks, added a bunch more overloads.

--- source/utility/tasks.d
+++ source/utility/tasks.d
@@ -23,9 +23,82 @@ void scheduleTask( bool delegate() dg )
  * Schedule a task to be executed until the duration expires.
  *
  * Params:
+ *  duration =          The duration to execute the task for
+ *  dg =                The task to execute
+ */
+void scheduleTimedTask( Duration duration, void delegate() dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        dg();
+        return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/// ditto
+void scheduleTimedTask( Duration duration, void delegate( float ) dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        dg( Time.totalTime - startTime );
+        return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/// ditto
+void scheduleTimedTask( Duration duration, void delegate( float, float ) dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        dg( Time.totalTime - startTime, duration.toSeconds );
+        return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/// ditto
+void scheduleTimedTask( Duration duration, bool delegate() dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        if( dg() )
+            return true;
+        else
+            return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/// ditto
+void scheduleTimedTask( Duration duration, bool delegate( float ) dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        if( dg( Time.totalTime - startTime ) )
+            return true;
+        else
+            return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/// ditto
+void scheduleTimedTask( Duration duration, bool delegate( float, float ) dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        if( dg( Time.totalTime - startTime, duration.toSeconds ) )
+            return true;
+        else
+            return Time.totalTime >= startTime + duration.toSeconds;
+    } );
+}
+
+/**
+ * Schedule a task to be executed until the duration expires.
+ *
+ * Params:
  *  dg =                The task to execute
  *  duration =          The duration to execute the task for
  */
+deprecated( "Use version with duration as first parameter." )
 void scheduleTimedTask( void delegate() dg, Duration duration )
 {
     auto startTime = Time.totalTime;
@@ -39,9 +112,33 @@ void scheduleTimedTask( void delegate() dg, Duration duration )
  * Schedule a task to be execuated after the specified amount of time.
  *
  * Params:
+ *  delay =             The ammount of time to wait before executing
+ *  dg =                The task to execute
+ */
+void scheduleDelayedTask( Duration delay, void delegate() dg )
+{
+    auto startTime = Time.totalTime;
+    scheduleTask( {
+        if( Time.totalTime - startTime >= delay.toSeconds )
+        {
+            dg();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } );
+}
+
+/**
+ * Schedule a task to be execuated after the specified amount of time.
+ *
+ * Params:
  *  dg =                The task to execute
  *  delay =             The ammount of time to wait before executing
  */
+deprecated( "Use version with delay as first parameter." )
 void scheduleDelayedTask( void delegate() dg, Duration delay )
 {
     auto startTime = Time.totalTime;
@@ -80,6 +177,9 @@ void executeTasks()
     }
 }
 
+/**
+ * Cancels all running tasks.
+ */
 void resetTasks()
 {
     scheduledTasks = [];

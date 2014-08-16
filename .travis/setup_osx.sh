62215b826f1dc9cc5905954b2fa37b65df13ca32
Colden Cullen <ColdenCullen@gmail.com>
2014-08-16 15:14:47 -0700

    Setup travis to manually install dmd and dub

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -1,7 +1,11 @@
 # Only run on OSX
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    brew update;
-    brew install dmd;
-    brew install dub;
+    # Install DMD
+    DMD_DMG=dmd.${DMD_VER}.dmg
+    wget ftp://ftp.dlang.org/${DMD_DMG}
+    sudo hdiutil mount ${DMD_DMG}
+    sudo installer -package /Volumes/DMD2/DMD2.pkg -target /
+
+    brew update
     brew install freeimage
 fi

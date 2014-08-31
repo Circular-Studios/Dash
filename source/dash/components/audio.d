25711714a45290e69bcfb86fb3d4ae2a074bf123
Sean Brennan <esean17@yahoo.com>
2014-08-31 17:51:07 -0400

    Added skeletons for audio listener and emitter, and added audio to the components package.

---
+++ source/dash/components/audio.d
@@ -0,0 +1,42 @@
+﻿/**
+ * Defines audio related classes, such as the listener and emitter
+ */
+module dash.components.audio;
+import dash.core.properties;
+import dash.components.component;
+import dash.utility.soloud;
+
+/**
+ * Listener object that hears sounds and sends them to the audio output device
+ * (usually attaced to the camera)
+ */
+class Listener : Component
+{
+private:
+
+public:
+	/**
+	 * Create a listener object 
+	 */
+	this()
+	{
+		// Constructor code
+	}
+}
+
+/**
+ * Emitter object that plays sounds that listeners can hear if they are close enough
+ */
+class Emitter : Component
+{
+private:
+
+public:
+	/**
+	 * Create an emmiter object
+	 */
+	this()
+	{
+		// Constructor Code
+	}
+}
